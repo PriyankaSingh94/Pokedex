55b21df9adb09dab90b8a7d83bef30f5bd5f2d90
Priyanka Singh <raopriyankasingh94@gmail.com>
2020-10-15 10:36:53 +0530

    Final Commit

--- src/app/pokemon-detail/pokemon-detail.component.ts
+++ src/app/pokemon-detail/pokemon-detail.component.ts
@@ -9,20 +9,37 @@ import {GetPokemonsService} from '../Services/get-pokemons.service';
 })
 export class PokemonDetailComponent implements OnInit {
   id: number;
-  editMode: boolean;
+  name: string;
+  hp: string;
+  attack: string;
+  defend: string;
+  pokemons: pokemon[];
   @Input() pokemon: pokemon;
   @Input() show: boolean;
+  @Input() editMode: boolean;
   @Output() onClose: EventEmitter<any> = new EventEmitter<any>();
 
   constructor(private getPokemonsService: GetPokemonsService) { }
 
   ngOnInit(): void {
-    //this.pokemon = this.getPokemonsService.getPokemonById(this.id);
   }
 
   closeDetails(){
     this.show = false;
     this.editMode = false;
     this.onClose.emit(this.show);
+  }
+
+  edit() {
+    this.pokemons = this.getPokemonsService.getPokemonsList();
+    for (let i = 0; i < this.pokemons.length; i++) {
+      if (this.pokemons[i].id == this.pokemon.id) {
+        this.pokemons[i].name = this.pokemon.name;
+        this.pokemons[i].hp = Number(this.pokemon.hp);
+        this.pokemons[i].attack = Number(this.pokemon.attack);
+        this.pokemons[i].defend = Number(this.pokemon.defend);
+      }
+    }
+    this.getPokemonsService.setPokimons(this.pokemons);
   }
 }
--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -8,15 +8,21 @@ import {GetPokemonsService} from '../Services/get-pokemons.service'
 })
 export class PokemonListComponent implements OnInit {
   show: boolean;
+  editMode: boolean;
+  hidePrev: boolean;
+  hideNext: boolean;
   pokemons: pokemon[] = [];
+  allPokemons: pokemon[] = [];
   selectedPokemon: pokemon;
+  startIndex: number = 0;
+  lastIndex: number = 9;
   constructor(private getPokemonsService: GetPokemonsService) { }
 
   ngOnInit(): void {
     this.getPokemonsService.getPokemons().subscribe(pokemons => {
-      this.pokemons = pokemons.slice(0, 5);
-      console.log(this.pokemons);
-      this.getPokemonsService.setPokimons(this.pokemons);
+      this.getPokemonsService.setPokimons(pokemons);
+      this.allPokemons = pokemons;
+      this.pokemons = pokemons.slice(this.startIndex, this.lastIndex);
     })
   }
 
@@ -25,7 +31,51 @@ export class PokemonListComponent implements OnInit {
     this.show = true;
   }
 
+  prevPage() {
+    this.startIndex-=9;
+    this.lastIndex-=9;
+    this.pokemons = this.allPokemons.slice(this.startIndex, this.lastIndex);
+    if (this.startIndex == 0) {
+      this.hidePrev = true;
+    }
+  }
+
+  nextPage() {
+    this.startIndex+=9;
+    this.lastIndex+=9;
+    this.pokemons = this.allPokemons.slice(this.startIndex, this.lastIndex);
+    if (this.lastIndex >= this.allPokemons.length) {
+      this.hideNext = true;
+    }
+  }
+
   hideDetails() {
     this.show = false;
+    this.editMode = false;
+    this.allPokemons = this.getPokemonsService.getPokemonsList();
+    this.showFirstPage();
+  }
+
+  editPokemon(pokemon) {
+    this.show = true;
+    this.editMode = true;
+    this.selectedPokemon = pokemon;
+  }
+
+  delete(id: number) {
+    for (let i = 0; i < this.allPokemons.length; i++) {
+      if (this.pokemons[i].id == id) {
+        this.allPokemons.splice(i, 1);
+        this.getPokemonsService.setPokimons(this.allPokemons);
+        this.showFirstPage();
+        break;
+      }
+    }
+  }
+
+  showFirstPage() {
+    this.startIndex = 0;
+    this.lastIndex = 9;
+    this.pokemons = this.allPokemons.slice(this.startIndex, this.lastIndex);
   }
 }
